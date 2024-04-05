43e3a22a4f5763e9b95169c543a6e4045d66d319
sungminna <sungmin.na330@gmail.com>
2024-04-05 11:23:50 +0900

    ojb_1931not done2

--- PS_Codes/boj_1931.cpp
+++ PS_Codes/boj_1931.cpp
@@ -1,18 +1,47 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+
 int main() {
-	unsigned int n, a, b;
+	unsigned int n;
 	std::cin >> n;
-	
+	unsigned int cnt = 0;
+
 	std::vector<std::pair<unsigned int, unsigned int>> data;
 	std::pair<unsigned int, unsigned int> pr;
 	for (unsigned int i = 0; i < n; i++) {
 		std::cin >> pr.first >> pr.second;
-		data.push_back(pr);
+		if (pr.first == pr.second) {
+			cnt++;
+		}
+		else {
+			data.push_back(pr);
+		}
 	}
 	std::sort(data.begin(), data.end());
+
+	unsigned int a;
+	unsigned int hit = data[0].first;
+	unsigned int min;
+	n -= cnt;
 	for (unsigned int i = 0; i < n; i++) {
-		std::cout << data[i].first << data[i].second;
+		a = data[i].first;
+		if (hit <= a) {
+			min = data[i].second;
+			for (unsigned int j = i; j < n; j++) {
+				if (a < data[j].first) {
+					break;
+				}
+				if (min > data[j].second) {
+					min = data[j].second;
+				}
+			}
+			std::cout << min;
+			hit = min;
+			cnt++;
+		}
+
 	}
+
+	std::cout << cnt;
 }
